a79cc690eb8c6e737f27c9c5bd56e4eeca08b870
Valentin <user@Users-MacBook-Pro.local>
2025-05-08 08:54:00 +0300

    add menu back

--- src/components/SidebarMenu/SidebarMenu.tsx
+++ src/components/SidebarMenu/SidebarMenu.tsx
@@ -1,20 +1,22 @@
 import Image from 'next/image'
+import useGetMenuList from '@/hooks/useGetMenuList'
 import { cn } from '@/library/utils'
 import { usePathname, useRouter } from 'next/navigation'
 import { useCategoriesQuery } from '@/queries/useCategoriesQuery'
 import { useTranslation } from 'react-i18next'
 
 const SidebarMenu = () => {
+  const menuList = useGetMenuList()
   const router = useRouter()
   const pathname = usePathname()
   const { i18n } = useTranslation()
-  const currentSlug = pathname.split('/').filter(Boolean).pop()
+  const id = pathname.split('/').filter(Boolean).pop()
 
   const { data: categories, isLoading } = useCategoriesQuery()
-
+  console.info(menuList)
   return (
     <div className='md:block h-[500px] sticky top-24 hidden border border-[#882727] rounded-2xl p-5 min-w-[258px]'>
-      {categories?.map((item, index) => {
+      {menuList?.map((item, index) => {
         return (
           <div
             key={index}
@@ -22,23 +24,16 @@ const SidebarMenu = () => {
             className={cn(
               'flex items-center gap-3 hover:font-bold hover:!text-[#882727] cursor-pointer mb-1',
               {
-                'font-bold !text-[#882727]': item.slug === currentSlug,
+                'font-bold !text-[#882727]': item.slug.toString() === id,
               },
             )}
           >
-{item?.image && (
-              <Image
-                src={process.env.NEXT_PUBLIC_BACK_END_URL + item.image}
-                width={22}
-                height={22}
-                alt={item.slug}
-              />
-            )}
+            <Image src={item?.iconSrc} width={22} height={22} alt={''} />
 
             <span>
               {
                 //@ts-ignore
-                item[`name_${i18n.language}`]
+                item.name
               }
             </span>
           </div>
