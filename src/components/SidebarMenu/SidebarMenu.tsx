2817eb0e7f72361700d8764ed52981495078554a
Valentin <user@Users-MacBook-Pro.local>
2025-05-08 12:37:23 +0300

    set up cetagories and cardme from api

--- src/components/SidebarMenu/SidebarMenu.tsx
+++ src/components/SidebarMenu/SidebarMenu.tsx
@@ -1,22 +1,21 @@
 import Image from 'next/image'
-import useGetMenuList from '@/hooks/useGetMenuList'
 import { cn } from '@/library/utils'
 import { usePathname, useRouter } from 'next/navigation'
 import { useCategoriesQuery } from '@/queries/useCategoriesQuery'
 import { useTranslation } from 'react-i18next'
 
 const SidebarMenu = () => {
-  const menuList = useGetMenuList()
   const router = useRouter()
   const pathname = usePathname()
   const { i18n } = useTranslation()
   const id = pathname.split('/').filter(Boolean).pop()
+  const currentSlug = pathname.split('/').filter(Boolean).pop()
 
   const { data: categories, isLoading } = useCategoriesQuery()
-  console.info(menuList)
+
   return (
     <div className='md:block h-[500px] sticky top-24 hidden border border-[#882727] rounded-2xl p-5 min-w-[258px]'>
-      {menuList?.map((item, index) => {
+      {categories?.map((item, index) => {
         return (
           <div
             key={index}
@@ -24,16 +23,23 @@ const SidebarMenu = () => {
             className={cn(
               'flex items-center gap-3 hover:font-bold hover:!text-[#882727] cursor-pointer mb-1',
               {
-                'font-bold !text-[#882727]': item.slug.toString() === id,
+                'font-bold !text-[#882727]': item.slug === currentSlug,
               },
             )}
           >
-            <Image src={item?.iconSrc} width={22} height={22} alt={''} />
+            {item?.image && (
+              <Image
+                src={process.env.NEXT_PUBLIC_BACK_END_URL + item.image}
+                width={22}
+                height={22}
+                alt={item.slug}
+              />
+            )}
 
             <span>
               {
                 //@ts-ignore
-                item.name
+                item[`name_${i18n.language}`]
               }
             </span>
           </div>
